dc3c5c2d2b76d14b1494b82dcb93a6bd0f36842b
asus-wolfcode-beetle <775121173@qq.com>
2018-08-21 15:38:55 +0800

    正常显示登陆信息

--- ihome/static/js/ihome/my.js
+++ ihome/static/js/ihome/my.js
@@ -1,8 +1,20 @@
+function getCookie(name) {
+    var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
+    return r ? r[1] : undefined;
+}
+
 function logout() {
-    $.get("/api/logout", function(data){
+    $.ajax({
+        url:"/api/v1.0/session", 
+        type:'delete',
+        headers:{'X-CSRFToken':getCookie('csrf_token')},
+        dataType:'json',
+        success:
+    function(data){
         if (0 == data.errno) {
             location.href = "/";
         }
+    }
     })
 }
 
