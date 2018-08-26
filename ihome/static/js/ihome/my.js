1b7d2eff73f024db0e18bda9d2d44711b1329a1a
lenovo-Recuer <775121173@qq.com>
2018-08-27 00:38:31 +0800

    OK !订单完成了,再快速看看支付

--- ihome/static/js/ihome/my.js
+++ ihome/static/js/ihome/my.js
@@ -3,20 +3,37 @@ function getCookie(name) {
     return r ? r[1] : undefined;
 }
 
+// 点击推出按钮时执行的函数
 function logout() {
     $.ajax({
-        url:"/api/v1.0/session", 
-        type:'delete',
-        headers:{'X-CSRFToken':getCookie('csrf_token')},
-        dataType:'json',
-        success:
-    function(data){
-        if (0 == data.errno) {
-            location.href = "/";
+        url: "/api/v1.0/session",
+        type: "delete",
+        headers: {
+            "X-CSRFToken": getCookie("csrf_token")
+        },
+        dataType: "json",
+        success: function (resp) {
+            if ("0" == resp.errno) {
+                location.href = "/index.html";
+            }
         }
-    }
-    })
+    });
 }
 
 $(document).ready(function(){
+    $.get("/api/v1.0/user", function(resp){
+        // 用户未登录
+        if ("4101" == resp.errno) {
+            location.href = "/login.html";
+        }
+        // 查询到了用户的信息
+        else if ("0" == resp.errno) {
+            $("#user-name").html(resp.data.name);
+            $("#user-mobile").html(resp.data.mobile);
+            if (resp.data.avatar) {
+                $("#user-avatar").attr("src", resp.data.avatar);
+            }
+
+        }
+    }, "json");
 })
